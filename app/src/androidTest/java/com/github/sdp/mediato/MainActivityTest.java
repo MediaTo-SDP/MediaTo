5e6a692485be538885ddf9ea548e3c3bad20c2b1
kalberma <simone.kalbermatter@epfl.ch>
2023-03-05 15:54:44 +0100

    Added MainActivity tests

--- app/src/androidTest/java/com/github/sdp/mediato/MainActivityTest.java
+++ app/src/androidTest/java/com/github/sdp/mediato/MainActivityTest.java
@@ -4,9 +4,12 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.*;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.espresso.ViewInteraction;
@@ -17,6 +20,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.assertTrue;
 
@@ -32,7 +36,26 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
 
     @Test
-    public void CheckMainGoButton() {
-        assertTrue(true);
+    public void testSelectHome() {
+        onView(withId(R.id.home)).perform(click());
+        onView(withId(R.id.frame_layout))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(withText("Home"))));
+    }
+
+    @Test
+    public void testSelectSearch() {
+        onView(withId(R.id.search)).perform(click());
+        onView(withId(R.id.frame_layout))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(withText("Search"))));
+    }
+
+    @Test
+    public void testSelectProfile() {
+        onView(withId(R.id.profile)).perform(click());
+        onView(withId(R.id.frame_layout))
+            .check(matches(isDisplayed()))
+            .check(matches(hasDescendant(withText("Profile"))));
     }
 }
