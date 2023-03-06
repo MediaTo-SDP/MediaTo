0e03d869a144bdce05971dd15d90c455982361a5
pierre hadrien levieil <pierre-hadrien.levieil@epfl.ch>
2023-03-06 17:02:24 +0100

    added slider indicator

--- app/src/main/java/com/github/sdp/mediato/NewItemActivity.java
+++ app/src/main/java/com/github/sdp/mediato/NewItemActivity.java
@@ -3,6 +3,8 @@ package com.github.sdp.mediato;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class NewItemActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class NewItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        SeekBar ratingSlider = findViewById(R.id.item_rating_slider);
+        TextView ratingIndicator = findViewById(R.id.item_rating_slider_progress);
+        ratingSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                ratingIndicator.setText("" + progress);
+                ratingIndicator.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                ratingIndicator.setY(seekBar.getY() - ratingIndicator.getTextSize()*1.5f);
+            }
+        });
     }
 }
