e039f90ae76eb5acd828d08a3b115f90a8a86456
pierre hadrien levieil <pierre-hadrien.levieil@epfl.ch>
2023-03-09 13:28:03 +0100

    added comments and code cleanup

--- app/src/androidTest/java/com/github/sdp/mediato/NewItemActivityTest.java
+++ app/src/androidTest/java/com/github/sdp/mediato/NewItemActivityTest.java
@@ -3,43 +3,56 @@ package com.github.sdp.mediato;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.init;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.hamcrest.core.AllOf.allOf;
-
-import android.content.Intent;
 import android.view.View;
 import android.widget.SeekBar;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.ViewAssertion;
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/**
+ * Test class for new item activity, used to add ratings and comments
+ */
 @RunWith(AndroidJUnit4.class)
 public class NewItemActivityTest {
 
     @Rule
     public ActivityScenarioRule<NewItemActivity> testRule = new ActivityScenarioRule<>(NewItemActivity.class);
+
+    // Changes the slide bar value to test it
+    private static ViewAction setProgress(final int progress) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((SeekBar) view).setProgress(progress);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set a progress on a SlideBar";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(SeekBar.class);
+            }
+        };
+    }
+
+    // Check if indicator text is well displayed when using the slide bar
     @Test
-    public void checkSlidebarAndIndicator(){
+    public void checkSlideBarAndIndicator() {
         init();
 
         ViewInteraction seekBar = onView(withId(R.id.item_rating_slider));
@@ -48,24 +61,6 @@ public class NewItemActivityTest {
         seekBar.perform(setProgress(5));
         seekBarIndicator.check(matches(withText("5")));
 
-    }
-
-
-    private static ViewAction setProgress(final int progress) {
-        return new ViewAction() {
-            @Override
-            public void perform(UiController uiController, View view) {
-                ((SeekBar) view).setProgress(progress);
-            }
-            @Override
-            public String getDescription() {
-                return "Set a progress on a SeekBar";
-            }
-            @Override
-            public Matcher<View> getConstraints() {
-                return ViewMatchers.isAssignableFrom(SeekBar.class);
-            }
-        };
     }
 
 }
--- app/src/main/java/com/github/sdp/mediato/NewItemActivity.java
+++ app/src/main/java/com/github/sdp/mediato/NewItemActivity.java
@@ -1,11 +1,15 @@
 package com.github.sdp.mediato;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * New item activity page, used to add a rating to an object as well as a review using a slide bar
+ * and edit text
+ */
 public class NewItemActivity extends AppCompatActivity {
 
     @Override
@@ -15,18 +19,26 @@ public class NewItemActivity extends AppCompatActivity {
 
         SeekBar ratingSlider = findViewById(R.id.item_rating_slider);
         TextView ratingIndicator = findViewById(R.id.item_rating_slider_progress);
+
+        // We add a listener to the slide bar to update the indicator text (displays the rating on
+        // top of it)
         ratingSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
+                // get position of slide bar and set the text to match rating and position
                 int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
                 ratingIndicator.setText("" + progress);
                 ratingIndicator.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
-                ratingIndicator.setY(seekBar.getY() - ratingIndicator.getTextSize()*1.5f);
+                ratingIndicator.setY(seekBar.getY() - ratingIndicator.getTextSize() * 1.5f);
             }
         });
     }
