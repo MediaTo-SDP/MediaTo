8be2499f75d012b3ffe707a6846306e43aa88494
sgueissa <stuart.gueissaz@epfl.ch>
2023-03-10 11:10:13 +0100

    Add old files to fix merge

---
+++ app/src/androidTest/java/com/github/sdp/mediato/MainActivityTest.java
@@ -0,0 +1,55 @@
+package com.github.sdp.mediato;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+  @Rule
+  public ActivityScenarioRule<MainActivity> testRule = new ActivityScenarioRule<MainActivity>(
+      MainActivity.class);
+
+  // Test that selecting Home on the bottom bar displays the HomeFragment
+  @Test
+  public void testSelectHome() {
+    onView(withId(R.id.home)).perform(click());
+    onView(withId(R.id.frame_layout))
+        .check(matches(isDisplayed()))
+        .check(matches(hasDescendant(withText("Home"))));
+  }
+
+  // Test that selecting Search on the bottom bar displays the SearchFragment
+  @Test
+  public void testSelectSearch() {
+    onView(withId(R.id.search)).perform(click());
+    onView(withId(R.id.frame_layout))
+        .check(matches(isDisplayed()))
+        .check(matches(hasDescendant(withText("Search"))));
+  }
+
+  // Test that selecting Profile on the bottom bar displays the ProfileFragment
+  @Test
+  public void testSelectProfile() {
+    onView(withId(R.id.profile)).perform(click());
+    onView(withId(R.id.frame_layout))
+        .check(matches(isDisplayed()))
+        .check(matches(hasDescendant(withText("Username"))));
+  }
+}
---
+++ app/src/main/java/com/github/sdp/mediato/MainActivity.java
@@ -0,0 +1,47 @@
+package com.github.sdp.mediato;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.github.sdp.mediato.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+  ActivityMainBinding binding;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    replaceFragment(new HomeFragment());
+    binding.bottomNavigationView.setBackground(null);
+
+    binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+      int itemId = item.getItemId();
+      // If/else statement is required instead if a switch case. See: http://tools.android.com/tips/non-constant-fields
+      if (itemId == R.id.home) {
+        replaceFragment(new HomeFragment());
+      } else if (itemId == R.id.search) {
+        replaceFragment(new SearchFragment());
+      } else if (itemId == R.id.profile) {
+        replaceFragment(new ProfileFragment());
+      }
+
+      return true;
+
+    });
+
+  }
+
+  private void replaceFragment(Fragment fragment) {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.frame_layout, fragment);
+    fragmentTransaction.commit();
+  }
+}
