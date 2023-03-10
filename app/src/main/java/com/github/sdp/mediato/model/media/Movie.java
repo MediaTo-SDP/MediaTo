a25a809f7ed82fe786de6314c4bb12e77a700d6e
MariemBaccari <mariembaccari70@gmail.com>
2023-03-10 10:39:21 +0100

    Adresses Simone's IRL comment about Movie constructor

--- app/src/main/java/com/github/sdp/mediato/model/media/Movie.java
+++ app/src/main/java/com/github/sdp/mediato/model/media/Movie.java
@@ -3,7 +3,7 @@ package com.github.sdp.mediato.model.media;
 import com.github.sdp.mediato.model.media.MediaType;
 
 public class Movie extends Media{
-    public Movie(MediaType mediaType, String title, String summary, String imageUrl) {
+    public Movie(String title, String summary, String imageUrl) {
         super(MediaType.MOVIE, title, summary, imageUrl);
     }
 
