557e79b940900601f518fe0aca7cb06090adb9f3
Pierre-Hadrien Levieil <pierre-hadrien.levieil@epfl.ch>
2023-03-11 16:10:18 +0100

    fixed a bug, changed the xml layout, added tests and character limit

--- app/src/androidTest/java/com/github/sdp/mediato/NewItemActivityTest.java
+++ app/src/androidTest/java/com/github/sdp/mediato/NewItemActivityTest.java
@@ -1,14 +1,20 @@
 package com.github.sdp.mediato;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.init;
+import static androidx.test.espresso.intent.Intents.release;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.github.sdp.mediato.NewItemActivity.MAX_REVIEW_LENGTH;
 
 import android.view.View;
 import android.widget.SeekBar;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
@@ -17,9 +23,13 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.hamcrest.Matcher;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Locale;
 
 /**
  * Test class for new item activity, used to add ratings and comments
@@ -29,6 +39,7 @@ public class NewItemActivityTest {
 
     @Rule
     public ActivityScenarioRule<NewItemActivity> testRule = new ActivityScenarioRule<>(NewItemActivity.class);
+    private View decorView;
 
     // Changes the slide bar value to test it
     private static ViewAction setProgress(final int progress) {
@@ -50,17 +61,69 @@ public class NewItemActivityTest {
         };
     }
 
+
+    @Before
+    public void setUp() {
+        init();
+        testRule.getScenario().onActivity(new ActivityScenario.ActivityAction<NewItemActivity>() {
+            @Override
+            public void perform(NewItemActivity activity) {
+                decorView = activity.getWindow().getDecorView();
+            }
+        });
+    }
+
     // Check if indicator text is well displayed when using the slide bar
     @Test
     public void checkSlideBarAndIndicator() {
-        init();
 
         ViewInteraction seekBar = onView(withId(R.id.item_rating_slider));
         ViewInteraction seekBarIndicator = onView(withId(R.id.item_rating_slider_progress));
 
         seekBar.perform(setProgress(5));
         seekBarIndicator.check(matches(withText("5")));
+    }
+
+    @Test
+    public void checkToastDisplayedWhenAddingACorrectLengthComment() {
+        ViewInteraction addButton = onView(withId(R.id.item_add_button));
+        addButton.perform(click());
+        // TODO: add tests when functionalities are implemented
 
     }
 
+    @Test
+    public void checkErrorMessageWhenAddingAIncorrectLengthComment() {
+        ViewInteraction addButton = onView(withId(R.id.item_add_button));
+        ViewInteraction editText = onView(withId(R.id.item_review_edittext));
+
+        editText.perform(typeText("A".repeat(MAX_REVIEW_LENGTH + 1)), closeSoftKeyboard());
+        addButton.perform(click());
+
+        onView(withId(R.id.new_item_review_error_msg))
+                .check(matches(withText(
+                        String.format(Locale.ENGLISH, "Exceeded character limit: %d", MAX_REVIEW_LENGTH))));
+    }
+
+    // After the error message is displayed, it should disappears when user edits the comment to make it shorter
+    // It reappears if the length is still to long when adding the review
+    @Test
+    public void checkErrorMessageDisappearsWhenEditing() {
+        ViewInteraction addButton = onView(withId(R.id.item_add_button));
+        ViewInteraction editText = onView(withId(R.id.item_review_edittext));
+
+        editText.perform(typeText("A".repeat(MAX_REVIEW_LENGTH + 1)), closeSoftKeyboard());
+        addButton.perform(click());
+        editText.perform(click(), closeSoftKeyboard());
+
+        onView(withId(R.id.new_item_review_error_msg))
+                .check(matches(withText("")));
+    }
+
+    @After
+    public void releaseIntents() {
+        release();
+    }
 }
+
+
--- app/src/main/java/com/github/sdp/mediato/NewItemActivity.java
+++ app/src/main/java/com/github/sdp/mediato/NewItemActivity.java
@@ -1,10 +1,16 @@
 package com.github.sdp.mediato;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 /**
  * New item activity page, used to add a rating to an object as well as a review using a slide bar
@@ -12,11 +18,50 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class NewItemActivity extends AppCompatActivity {
 
+    // The maximum allowed length for review field
+    public final static int MAX_REVIEW_LENGTH = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
 
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        // set the item title, description, etc
+        // for now, only mock values before linking to database
+        setItemInformation("OSS 117", "OSS 117 : Alerte rouge en Afrique noire ou" +
+                " OSS 117: Bons Baisers d'Afrique au Québec est une comédie d'espionnage" +
+                " française réalisé par Nicolas Bedos et sorti en 2021.", R.drawable.oss117_item_test);
+
+        setProgressBarIndicator();
+
+        TextView errorTextView = findViewById(R.id.new_item_review_error_msg);
+        Button itemAddButton = findViewById(R.id.item_add_button);
+        EditText review = findViewById(R.id.item_review_edittext);
+
+        // Set the "add" button to display an error when character limit is exceeded
+        itemAddButton.setOnClickListener(
+                v -> {
+                    if (review.getText().length() > MAX_REVIEW_LENGTH) {
+                        errorTextView.setText(
+                                String.format(Locale.ENGLISH, "Exceeded character limit: %d", MAX_REVIEW_LENGTH));
+                    }
+                });
+
+        // Clear the error message (if there is one) once the user edits the text
+        review.setOnClickListener(v -> {
+            if (errorTextView.getText().length() > 0) {
+                errorTextView.setText("");
+            }
+        });
+
+    }
+
+    /**
+     * Adds a listener to the progress bar in order to attach the indicator above (displays the rating)
+     */
+    private void setProgressBarIndicator() {
         SeekBar ratingSlider = findViewById(R.id.item_rating_slider);
         TextView ratingIndicator = findViewById(R.id.item_rating_slider_progress);
 
@@ -36,10 +81,27 @@ public class NewItemActivity extends AppCompatActivity {
 
                 // get position of slide bar and set the text to match rating and position
                 int val = (progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
-                ratingIndicator.setText("" + progress);
-                ratingIndicator.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2);
+                ratingIndicator.setText(String.valueOf(progress));
+                ratingIndicator.setX(seekBar.getX() + val + seekBar.getThumbOffset() / 2.f);
                 ratingIndicator.setY(seekBar.getY() - ratingIndicator.getTextSize() * 1.5f);
             }
         });
+    }
+
+    /**
+     * Sets the on-screen title and description to match those given as parameters
+     *
+     * @param title:       the item title
+     * @param description: the item description
+     * @param image_res:   the item image resource link
+     */
+    private void setItemInformation(String title, String description, int image_res) {
+
+        ((TextView) findViewById(R.id.item_title)).setText(title);
+
+        ((TextView) findViewById(R.id.item_description_text)).setText(description);
+
+        ((ImageView) findViewById(R.id.item_image)).setImageResource(image_res);
+
     }
 }
