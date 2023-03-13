77811695d473de26933ff2e76fb5495d51631ba8
Pierre-Hadrien Levieil <pierre-hadrien.levieil@epfl.ch>
2023-03-13 19:06:52 +0100

    changed cirrus config

--- app/src/androidTest/java/com/github/sdp/mediato/AuthenticationActivityTest.java
+++ app/src/androidTest/java/com/github/sdp/mediato/AuthenticationActivityTest.java
@@ -123,14 +123,14 @@ public class AuthenticationActivityTest {
 
         ViewInteraction loginButton = onView(withId(R.id.google_sign_in));
         loginButton.perform(click());
-        Thread.sleep(3000);
+        Thread.sleep(30000);
         try {
             device.findObject(By.textContains("@")).click();
         } catch (NullPointerException e) {
             System.out.println("Object wasn't found");
         }
 
-        Thread.sleep(3000);
+        Thread.sleep(30000);
         Intents.intended(IntentMatchers.hasComponent(GreetingActivity.class.getName()));
 
         logout();
