91775d77b270e3d8a8abe839dbdf8b520aacff19
kalberma <simone.kalbermatter@epfl.ch>
2023-03-16 00:02:55 +0100

    added functions to set profile picture and username in profile

--- app/src/main/java/com/github/sdp/mediato/ProfileFragment.java
+++ app/src/main/java/com/github/sdp/mediato/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.github.sdp.mediato;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +14,9 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import com.github.sdp.mediato.data.Database;
 import com.github.sdp.mediato.utility.PhotoPicker;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * A fragment that displays the user's profile information, including their profile picture,
@@ -21,23 +25,25 @@ import com.github.sdp.mediato.utility.PhotoPicker;
  */
 public class ProfileFragment extends Fragment {
 
-  private ImageView profileImage;
   private PhotoPicker photoPicker;
+
+  // this string is used to access the user's data in the database
+  private String username = "Username";
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-    // Get all user interface components
+    // Get all UI components
     Button edit_button = view.findViewById(R.id.edit_button);
     AppCompatImageButton add_movie_button = view.findViewById(R.id.add_movie_button);
-    TextView username_tv = view.findViewById(R.id.username_text);
-    profileImage = view.findViewById(R.id.profile_image);
+    TextView username_view = view.findViewById(R.id.username_text);
+    ImageView profileImage = view.findViewById(R.id.profile_image);
 
-    // Get the username set by the profile creation page and forwarded by the MainActivity
-    String username = getArguments().getString("username");
-    username_tv.setText(username);
+    setUsername(username_view);
+    // This does not work yet
+    //setProfileImage(profileImage);
 
     // On click on the edit button, open a photo picker to choose the profile image
     photoPicker = new PhotoPicker(this, profileImage);
@@ -61,5 +67,27 @@ public class ProfileFragment extends Fragment {
     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
     fragmentTransaction.replace(R.id.main_container, fragment);
     fragmentTransaction.commit();
+  }
+
+  private void setProfileImage(ImageView profileImage) {
+    CompletableFuture<byte[]> imageFuture = Database.getProfilePic(username);
+
+    imageFuture.thenAccept(imageBytes -> {
+      // Create a Bitmap object from the byte array
+      Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+
+      // Set the bitmap to the ImageView
+      profileImage.setImageBitmap(bitmap);
+    });
+  }
+
+
+  /**
+   * For now this gets the username set by the profile creation page and forwarded by the
+   * MainActivity, but this might be changed in the future when the user is already logged in.
+   */
+  private void setUsername(TextView username) {
+    String arg = getArguments().getString("username");
+    username.setText(arg);
   }
 }
