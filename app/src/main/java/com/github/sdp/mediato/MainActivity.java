b6e152ed16c34f0096ecbec36d07b4d0503a96a7
kalberma <simone.kalbermatter@epfl.ch>
2023-03-15 16:32:12 +0100

    refactor and comment MainActivity

--- app/src/main/java/com/github/sdp/mediato/MainActivity.java
+++ app/src/main/java/com/github/sdp/mediato/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.github.sdp.mediato.databinding.ActivityMainBinding;
 
+/**
+ * The main activity of the app that displays a bottom navigation bar and manages the navigation
+ * between the home, search, and profile fragments.
+ */
 public class MainActivity extends AppCompatActivity {
 
   ActivityMainBinding binding;
@@ -17,25 +21,28 @@ public class MainActivity extends AppCompatActivity {
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
+    // Choose the default fragment that opens on creation of the MainActivity
     replaceFragment(new HomeFragment());
+
+    // Set the bottomNavigationView
     binding.bottomNavigationView.setBackground(null);
+    binding.bottomNavigationView.setOnItemSelectedListener(
+        item -> navigateFragments(item.getItemId()));
 
-    binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+  }
 
-      int itemId = item.getItemId();
-      // If/else statement is required instead if a switch case. See: http://tools.android.com/tips/non-constant-fields
-      if (itemId == R.id.home) {
-        replaceFragment(new HomeFragment());
-      } else if (itemId == R.id.search) {
-        replaceFragment(new SearchFragment());
-      } else if (itemId == R.id.profile) {
-        replaceFragment(new ProfileFragment());
-      }
+  private boolean navigateFragments(int itemId) {
+    // If/else statement is required instead if a switch case.
+    // See: http://tools.android.com/tips/non-constant-fields
+    if (itemId == R.id.home) {
+      replaceFragment(new HomeFragment());
+    } else if (itemId == R.id.search) {
+      replaceFragment(new SearchFragment());
+    } else if (itemId == R.id.profile) {
+      replaceFragment(new ProfileFragment());
+    }
 
-      return true;
-
-    });
-
+    return true;
   }
 
   private void replaceFragment(Fragment fragment) {
