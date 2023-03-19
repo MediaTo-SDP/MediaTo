4378789821b7b12a191aab2e669a2f23a79fe4ca
MariemBaccari <mariembaccari70@gmail.com>
2023-03-19 10:45:09 +0100

    Adds following user tests

--- app/src/androidTest/java/com/github/sdp/mediato/DatabaseTests.java
+++ app/src/androidTest/java/com/github/sdp/mediato/DatabaseTests.java
@@ -5,52 +5,105 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
+import androidx.annotation.NonNull;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.github.sdp.mediato.data.Database;
 import com.github.sdp.mediato.model.Location;
 import com.github.sdp.mediato.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+
 @RunWith(AndroidJUnit4.class)
+
 /**
  * This class contains all the tests for database interactions
+ *
  * @TODO add the Cloud Storage tests for the profile pictures
  */
 public class DatabaseTests {
     private static int STANDARD_TIMEOUT = 10;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     User user1;
+    User user2;
+    User user3;
 
     @Before
-    public void setUp(){
-       try {
-           Database.database.useEmulator("10.0.2.2", 9000);
+    public void setUp() {
+        try {
+            Database.database.useEmulator("10.0.2.2", 9000);
+        } catch (Exception e) {
         }
-        catch(Exception e){}
-        //Create new sample user
+        //Create new sample users
         user1 = new User.UserBuilder("uniqueId1")
                 .setUsername("user_test_1")
                 .setEmail("email_test_1")
                 .setRegisterDate("09/03/2023")
                 .setLocation(new Location(3.14, 3.14))
                 .build();
+        user2 = new User.UserBuilder("uniqueId2")
+                .setUsername("user_test_2")
+                .setEmail("email_test_2")
+                .setRegisterDate("19/03/2023")
+                .setLocation(new Location(3.14, 3.14))
+                .build();
+        user3 = new User.UserBuilder("uniqueId3")
+                .setUsername("user_test_3")
+                .setEmail("email_test_3")
+                .setRegisterDate("19/03/2023")
+                .setLocation(new Location(3.14, 3.14))
+                .build();
     }
 
-    @After
-    //Cleans database values after each test
-    public void cleanDatabase(){
-        Database.database.getReference().setValue(null);
+    @Test
+    //Tests that following a user adds the right username in the following list and the followers list
+    public void followUserAddsUsernameInFollowingAndFollowers() throws ExecutionException, InterruptedException, TimeoutException {
+        Database.addUser(user2).get(STANDARD_TIMEOUT, TimeUnit.SECONDS);
+        Database.addUser(user3).get(STANDARD_TIMEOUT, TimeUnit.SECONDS);
+        Database.followUser(user2.getUsername(), user3.getUsername());
+        DatabaseReference user1FollowingRef = Database.database.getReference().child(Database.USER_PATH + user2.getUsername() + Database.FOLLOWING_PATH);
+        user1FollowingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Map<String, Boolean> following = (Map<String, Boolean>) snapshot.getValue();
+                assertTrue(following.containsKey(user3.getUsername()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                throw error.toException();
+            }
+        });
+
+        DatabaseReference user2FollowersRef = Database.database.getReference().child(Database.USER_PATH + user3.getUsername() + Database.FOLLOWERS_PATH);
+        user2FollowersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Map<String, Boolean> followers = (Map<String, Boolean>) snapshot.getValue();
+                assertTrue(followers.containsKey(user2.getUsername()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                throw error.toException();
+            }
+        });
     }
 
     @Test
@@ -62,7 +115,7 @@ public class DatabaseTests {
         User retrievedUserByEmail = Database.getUserByEmail(user1.getEmail()).get(STANDARD_TIMEOUT, TimeUnit.SECONDS);
         List<User> retrievedUsers = List.of(retrievedUserByUsername, retrievedUserByEmail);
 
-        for (User retrievedUser: retrievedUsers) {
+        for (User retrievedUser : retrievedUsers) {
             assertEquals(retrievedUser.getUsername(), user1.getUsername());
             assertEquals(retrievedUser.getLocation().getLatitude(), user1.getLocation().getLatitude(), 0);
             assertEquals(retrievedUser.getLocation().getLongitude(), user1.getLocation().getLongitude(), 0);
@@ -74,22 +127,22 @@ public class DatabaseTests {
 
     @Test
     //Tests that trying to retrieve a non existent user throws an exception
-    public void gettingNonExistentUserThrowsException(){
+    public void gettingNonExistentUserThrowsException() {
         assertThrows(
-                Exception.class, ()-> Database.getUser("imaginary user").get(STANDARD_TIMEOUT, TimeUnit.SECONDS)
+                Exception.class, () -> Database.getUser("imaginary user").get(STANDARD_TIMEOUT, TimeUnit.SECONDS)
         );
         assertThrows(
-                Exception.class, ()-> Database.getUserByEmail("imaginary email").get(STANDARD_TIMEOUT, TimeUnit.SECONDS)
+                Exception.class, () -> Database.getUserByEmail("imaginary email").get(STANDARD_TIMEOUT, TimeUnit.SECONDS)
         );
-
     }
 
     @Test
     //Tests that database properly removes a user
     public void deletingRemovesUserFromDatabase() throws ExecutionException, InterruptedException, TimeoutException {
+        Database.addUser(user1).get(STANDARD_TIMEOUT, TimeUnit.SECONDS);
         Database.deleteUser(user1.getUsername()).get(STANDARD_TIMEOUT, TimeUnit.SECONDS);
         assertThrows(
-                Exception.class, ()-> Database.getUser(user1.getUsername()).get(STANDARD_TIMEOUT, TimeUnit.SECONDS)
+                Exception.class, () -> Database.getUser(user1.getUsername()).get(STANDARD_TIMEOUT, TimeUnit.SECONDS)
         );
     }
 
