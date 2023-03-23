94040a1b4f88baf1f4a3fbc100f9d9c94a746260
kalberma <simone.kalbermatter@epfl.ch>
2023-03-23 04:27:36 +0100

    fixed mistake in merge

--- app/src/main/java/com/github/sdp/mediato/AuthenticationActivity.java
+++ app/src/main/java/com/github/sdp/mediato/AuthenticationActivity.java
@@ -29,80 +29,83 @@ public class AuthenticationActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_authentication);
 
-        // We assign a callback to Google sign in button
-        findViewById(R.id.google_sign_in).setOnClickListener(view -> {
-            FirebaseUser authUser = FirebaseAuth.getInstance().getCurrentUser();
-            if (authUser == null) {
-                // Initialize sign in intent
-                Intent signInIntent = AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(Collections.singletonList(
-                                new AuthUI.IdpConfig.GoogleBuilder().build())) // only available login is Google
-                        .build();
-                // Start the intent
-                signInLauncher.launch(signInIntent);
-            } else {
-                launchPostActivity(authUser);
-            }
-        });
+    // We assign a callback to Google sign in button
+    findViewById(R.id.google_sign_in).setOnClickListener(view -> {
+      FirebaseUser authUser = FirebaseAuth.getInstance().getCurrentUser();
+      if (authUser == null) {
+        // Initialize sign in intent
+        Intent signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(Collections.singletonList(
+                new AuthUI.IdpConfig.GoogleBuilder().build())) // only available login is Google
+            .build();
+        // Start the intent
+        signInLauncher.launch(signInIntent);
+      } else {
+        launchPostActivity(authUser);
+      }
+    });
 
   }
 
 
-    /**
-     * Called when user is login in. If login is successful, the user is greeted.
-     *
-     * @param result: firebase authentication result
-     */
-    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+  /**
+   * Called when user is login in. If login is successful, the user is greeted.
+   *
+   * @param result: firebase authentication result
+   */
+  private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
 
-        if (result.getResultCode() == RESULT_OK) {
-            // Successfully signed in
-            // launch greeting activity with user's name
-            launchPostActivity(
-                    Objects.requireNonNull(
-                            FirebaseAuth.getInstance().getCurrentUser()));
-        }
+    if (result.getResultCode() == RESULT_OK) {
+      // Successfully signed in
+      // launch greeting activity with user's name
+      launchPostActivity(
+          Objects.requireNonNull(
+              FirebaseAuth.getInstance().getCurrentUser()));
     }
   }
+  
 
-    /**
-     * Launches the next activity with the user signed in, either the main if user exists in database,
-     * or the profile creation one otherwise
-     *
-     * @param user: user's name
-     */
-    public void launchPostActivity(FirebaseUser user) {
-        Objects.requireNonNull(user);
+  /**
+   * Launches the next activity with the user signed in, either the main if user exists in database,
+   * or the profile creation one otherwise
+   *
+   * @param user: user's name
+   */
+  public void launchPostActivity(FirebaseUser user) {
+    Objects.requireNonNull(user);
 
-        Database.getUserByEmail(user.getEmail()).thenAccept(this::launchMainActivity).exceptionally(e -> {
-            launchProfileCreationActivity(user);
-            return null;
+    Database.getUserByEmail(user.getEmail()).thenAccept(this::launchMainActivity)
+        .exceptionally(e -> {
+          launchProfileCreationActivity(user);
+          return null;
         });
-    }
+  }
 
-    /**
-     * Launches the main activity when the user already has a profile
-     * @param databaseUser: the user's profile from the database
-     */
-    private void launchMainActivity(User databaseUser) {
-        Intent postIntent = new Intent(AuthenticationActivity.this, MainActivity.class);
-        postIntent.putExtra("username", databaseUser.getUsername());
-        AuthenticationActivity.this.startActivity(postIntent);
+  /**
+   * Launches the main activity when the user already has a profile
+   *
+   * @param databaseUser: the user's profile from the database
+   */
+  private void launchMainActivity(User databaseUser) {
+    Intent postIntent = new Intent(AuthenticationActivity.this, MainActivity.class);
+    postIntent.putExtra("username", databaseUser.getUsername());
+    AuthenticationActivity.this.startActivity(postIntent);
 
-    }
+  }
 
-    /**
-     * Launches the profile creation activity when the user doesn't have one
-     * @param user: the authenticated user
-     */
-    private void launchProfileCreationActivity(FirebaseUser user) {
-        Intent postIntent = new Intent(AuthenticationActivity.this, NewProfileActivity.class);
-        postIntent.putExtra("uid", user.getUid());
-        postIntent.putExtra("email", user.getEmail());
-        AuthenticationActivity.this.startActivity(postIntent);
+  /**
+   * Launches the profile creation activity when the user doesn't have one
+   *
+   * @param user: the authenticated user
+   */
+  private void launchProfileCreationActivity(FirebaseUser user) {
+    Intent postIntent = new Intent(AuthenticationActivity.this, NewProfileActivity.class);
+    postIntent.putExtra("uid", user.getUid());
+    postIntent.putExtra("email", user.getEmail());
+    AuthenticationActivity.this.startActivity(postIntent);
 
-    }
+  }
 
 
 }
