e8504470006c01d491f8ace6097fdb1831da38df
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2023-03-27 11:30:06 +0200

    search menu refractored to fragment

---
+++ app/src/main/java/com/github/sdp/mediato/MediaAdapter.java
@@ -0,0 +1,72 @@
+package com.github.sdp.mediato;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.github.sdp.mediato.model.media.Media;
+
+import java.util.List;
+
+public class MediaAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Media> mediaItems;
+
+    private LayoutInflater inflater;
+
+    public MediaAdapter(Context context, List<Media> mediaItems) {
+        this.context = context;
+        this.mediaItems = mediaItems;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mediaItems.size();
+    }
+
+    @Override
+    public Media getItem(int position) {
+        return mediaItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        convertView = inflater.inflate(R.layout.adapter_item, null);
+
+        Media currentItem = getItem(position);
+        String name = currentItem.getTitle();
+        String src = currentItem.getImageURL();
+
+        // to do change the image, fetched from url
+        ImageView moviePoster = convertView.findViewById(R.id.item_icon);
+        
+        // toDO : fetch from the dataBase
+        int resId = context.getResources().getIdentifier(src, "drawable", context.getPackageName());
+        moviePoster.setImageResource(resId);
+
+        TextView movieName = convertView.findViewById(R.id.item_name);
+        movieName.setText(name);
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // do the job
+            }
+        });
+        return convertView;
+    }
+}
--- app/src/main/java/com/github/sdp/mediato/SearchFragment.java
+++ app/src/main/java/com/github/sdp/mediato/SearchFragment.java
@@ -1,38 +1,144 @@
 package com.github.sdp.mediato;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
 import androidx.fragment.app.Fragment;
+
+import com.github.sdp.mediato.model.LocalFilmDatabase;
+import com.github.sdp.mediato.model.media.Media;
+
 import java.util.ArrayList;
+import java.util.List;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements View.OnClickListener, SearchView.OnQueryTextListener {
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-      Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.fragment_search, container, false);
+    private enum SearchCategory {
+        PEOPLE,
+        MOVIES,
+        BOOKS,
+        MUSIC
+    }
 
-    // Get a reference to the ListView
-    ListView listView = view.findViewById(R.id.searchactivity_listview_searchresults);
+    private SearchView searchView;
+    private TextView textView;
+    private GridView gridView;
 
-// Initialize the adapter and set it as the adapter for the ListView
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
-        android.R.layout.simple_list_item_1, new ArrayList<String>());
-    listView.setAdapter(adapter);
+    private Button peopleButton;
+    private Button booksButton;
+    private Button filmButton;
+    private Button musicButton;
 
-// Add an item to the adapter
-    adapter.add("New Item");
+    private SearchFragment.SearchCategory currentCategory;
+    private Button currentHighlightedButton;
 
-// Notify the adapter that the data has changed
-    adapter.notifyDataSetChanged();
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-    return view;
+        // Inflate the layout for the search fragment
+        View searchView = inflater.inflate(R.layout.search_menu, container, false);
 
-  }
+        this.peopleButton = searchView.findViewById(R.id.searchactivity_setCategorie_people);
+        peopleButton.setOnClickListener(this);
 
+        this.booksButton = searchView.findViewById(R.id.searchactivity_setCategorie_books);
+        booksButton.setOnClickListener(this);
+
+        this.filmButton = searchView.findViewById(R.id.searchactivity_setCategorie_film);
+        filmButton.setOnClickListener(this);
+
+        this.musicButton = searchView.findViewById(R.id.searchactivity_setCategorie_music);
+        musicButton.setOnClickListener(this);
+
+        // get the text view
+        this.textView = searchView.findViewById(R.id.searchactivity_textView_textDuringAfterSearch);
+
+        // get the list view
+        this.gridView = searchView.findViewById(R.id.searchactivity_gridview_searchresults);
+
+        // get the search view and bind it to a listener
+        this.searchView = searchView.findViewById(R.id.searchactivity_searchview_searchbar);
+        this.searchView.setOnQueryTextListener(this);
+
+        // finally warmup the system, the activity starts by searching for peoples
+        this.currentHighlightedButton = peopleButton;
+        this.currentHighlightedButton.setPaintFlags(this.currentHighlightedButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        this.currentCategory = SearchFragment.SearchCategory.PEOPLE;
+
+        return searchView;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        if (s.length() > 0) {
+            // this.mTextView.setText("Results");
+            // List<Media> searchResults = search(s);
+            // gridView.setAdapter(new MediaAdapter(this.getContext(), searchResults));
+
+            // toDO : call the search function
+            // toDO : update the GridView
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        if (s.length() > 0) {
+            // this.mTextView.setText("Suggested");
+            //search(s);
+            // toDO : call the search function
+            // toDO : update the GridView
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == peopleButton) {
+            this.currentCategory = SearchFragment.SearchCategory.PEOPLE;
+        } else if (view == booksButton) {
+            this.currentCategory = SearchFragment.SearchCategory.BOOKS;
+        } else if (view == filmButton) {
+            this.currentCategory = SearchFragment.SearchCategory.MOVIES;
+        } else if (view == musicButton) {
+            this.currentCategory = SearchFragment.SearchCategory.MUSIC;
+        }
+
+        this.currentHighlightedButton.setTypeface(null, Typeface.NORMAL);
+        this.currentHighlightedButton.setPaintFlags(this.currentHighlightedButton.getPaintFlags() & (~Paint.UNDERLINE_TEXT_FLAG));
+        this.currentHighlightedButton = (Button) view;
+        this.currentHighlightedButton.setTypeface(null, Typeface.BOLD);
+        this.currentHighlightedButton.setPaintFlags(this.currentHighlightedButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    }
+
+    private List<Media> search(String toBeSearched) {
+        List<Media> searchMediaResults = new ArrayList<Media>();
+        switch (this.currentCategory) {
+            case PEOPLE:
+                break;
+            case MOVIES:
+
+                // toDO : fetch from firebase 
+                // for now fetches from a local database
+                if (toBeSearched.equals("james bond")) {
+                    searchMediaResults = new LocalFilmDatabase().getMovieItems();
+                }
+                break;
+            case BOOKS:
+                break;
+            case MUSIC:
+                break;
+        }
+        return searchMediaResults;
+    }
 }
