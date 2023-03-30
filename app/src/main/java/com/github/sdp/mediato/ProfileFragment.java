1100eddf75491be57882984833c50d2a62c7fbea
stuart <stuart.gueissaz@epfl.ch>
2023-03-30 21:18:31 +0200

    Add missing imports

--- app/src/main/java/com/github/sdp/mediato/ProfileFragment.java
+++ app/src/main/java/com/github/sdp/mediato/ProfileFragment.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
