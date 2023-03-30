34feda1d0df3c3142eb9413431f221b5019230ef
kalberma <simone.kalbermatter@epfl.ch>
2023-03-30 17:30:13 +0200

    use db emulator

--- app/src/androidTest/java/com/github/sdp/mediato/ProfileFragmentTest.java
+++ app/src/androidTest/java/com/github/sdp/mediato/ProfileFragmentTest.java
@@ -22,6 +22,7 @@ import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
+import com.github.sdp.mediato.data.Database;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.junit.Before;
@@ -48,6 +49,12 @@ public class ProfileFragmentTest {
   public void setUp() {
     // Launch the TestingActivity
     scenario = ActivityScenario.launch(TestingActivity.class);
+
+    // Use Database emulator
+    try {
+      Database.database.useEmulator("10.0.2.2", 9000);
+    } catch (Exception ignored) {
+    }
 
     // Set up the TestingActivity to display the ProfileFragment
     scenario.onActivity(activity -> {
