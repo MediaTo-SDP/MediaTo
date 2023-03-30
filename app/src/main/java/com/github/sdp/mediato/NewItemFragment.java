c8b11e224f855b63811521ae7b59dd2b35b45c15
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2023-03-30 10:35:44 +0200

    Film Review element refractored into a Fragment and slightly modified to fit the use

---
+++ app/src/main/java/com/github/sdp/mediato/NewItemFragment.java
@@ -0,0 +1,13 @@
+package com.github.sdp.mediato;
+
+import android.view.View;
+
+import androidx.fragment.app.Fragment;
+
+public class NetItemFragment extends Fragment {
+
+
+    protected View onCreateView() {
+        
+    }
+}
