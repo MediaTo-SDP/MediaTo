98681643ef2610bf9c84ee372f67b4e46a138a05
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2023-04-21 05:09:57 +0200

    - ask the user for the permission to use the gps location and when location is enabled, send regularly the current location to firebase

--- app/src/main/java/com/github/sdp/mediato/MainActivity.java
+++ app/src/main/java/com/github/sdp/mediato/MainActivity.java
@@ -1,24 +1,40 @@
 package com.github.sdp.mediato;
 
-import android.os.Bundle;
+import static com.github.sdp.mediato.data.UserDatabase.updateLocation;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import androidx.lifecycle.ViewModelProvider;
+
 import com.github.sdp.mediato.databinding.ActivityMainBinding;
 import com.github.sdp.mediato.ui.HomeFragment;
 import com.github.sdp.mediato.ui.MyProfileFragment;
 import com.github.sdp.mediato.ui.SearchFragment;
 import com.github.sdp.mediato.ui.viewmodel.ProfileViewModel;
 
+import java.util.List;
+
 /**
  * The main activity of the app that displays a bottom navigation bar and manages the navigation
  * between the home, search, and profile fragments.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
   ActivityMainBinding binding;
   MyProfileFragment myProfileFragment;
@@ -26,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
   private ProfileViewModel currentUserViewModel;
   private ProfileViewModel otherUsersViewModel;
+
+  private LocationManager locationManager;
+  private Boolean locationEnabled;
+  private String username;
+
+  private final int LOCATION_REQUEST = 0;
+  private final int UPDATE_LOCATION_EACH = 900000; // 900 000ms for 15min
+  private final int UPDATE_LOCATION_ON_POSITION_CHANGE_OF = 1; // 1M
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +67,47 @@ public class MainActivity extends AppCompatActivity {
     // Set the bottomNavigationView
     binding.bottomNavigationView.setBackground(null);
     binding.bottomNavigationView.setOnItemSelectedListener(
-        item -> navigateFragments(item.getItemId()));
+            item -> navigateFragments(item.getItemId()));
 
+    // after login register the position of the user
+    locationEnabled = true;
+    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+      // request permission to access the location
+      Toast.makeText(MainActivity.this, "gps location not allowed", Toast.LENGTH_SHORT).show();
+      this.locationEnabled = false;
+      ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+              android.Manifest.permission.ACCESS_COARSE_LOCATION,
+              android.Manifest.permission.ACCESS_FINE_LOCATION
+      }, LOCATION_REQUEST);
+    }
+
+    if(locationEnabled){
+      this.locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_LOCATION_EACH, UPDATE_LOCATION_ON_POSITION_CHANGE_OF,MainActivity.this);
+    }
+  }
+
+  @Override
+  public void onLocationChanged(@NonNull Location location) {
+    Toast.makeText(getApplicationContext(), "new location : " + location.getLatitude() + "," + location.getLongitude(), Toast.LENGTH_SHORT).show();
+    updateLocation(username, location.getLatitude(), location.getLongitude());
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    if(requestCode == LOCATION_REQUEST){
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        // Showing the toast message
+        Toast.makeText(MainActivity.this, "location permission granted, gps location service running in the background", Toast.LENGTH_SHORT).show();
+        this.locationEnabled = true;
+      }else {
+        Toast.makeText(MainActivity.this, "location permission denied", Toast.LENGTH_SHORT).show();
+        this.locationEnabled = false;
+      }
+    }
   }
 
   private boolean navigateFragments(int itemId) {
@@ -78,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
     searchFragment = new SearchFragment();
 
     // Get the username set by the profile creation activity
-    String username = getIntent().getStringExtra("username");
+    username = getIntent().getStringExtra("username");
     Bundle args = new Bundle();
 
     // Give the username as an argument to the profile page and switch to it
@@ -98,5 +161,13 @@ public class MainActivity extends AppCompatActivity {
 
   public ProfileViewModel getOtherUsersViewModel(){
     return otherUsersViewModel;
+  }
+
+  public String getCurrentUserUsername(){
+    return username;
+  }
+
+  public Boolean isLocationEnabled(){
+    return locationEnabled;
   }
 }
