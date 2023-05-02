ec64e55f1f44a200447068c577c1a6de9b7f0b33
Simone Kalbermatter <simone.kalbermatter@epfl.ch>
2023-05-02 17:21:12 +0200

    refactor

--- app/src/main/java/com/github/sdp/mediato/ui/MyProfileFragment.java
+++ app/src/main/java/com/github/sdp/mediato/ui/MyProfileFragment.java
@@ -79,17 +79,7 @@ public class MyProfileFragment extends BaseProfileFragment {
         signOutButton.setOnClickListener(v -> ((MainActivity) getActivity()).signOutUser());
 
         // Add a review if there is one
-        Bundle args = getArguments();
-        String reviewSerialized = args.getString("review");
-        String collectionName = args.getString("collection");
-        Review review = new Gson().fromJson(reviewSerialized, Review.class);
-        System.out.println("bundle in main: " + bundle);
-
-        if (review != null && collectionName != null) {
-            // TODO: add to the right collection
-            ((MyProfileViewModel)viewModel).addReviewToCollection(review, collectionName);
-            CollectionsDatabase.addReviewToCollection(USERNAME, collectionName, review);
-        }
+        addReview();
 
         return view;
     }
@@ -99,29 +89,17 @@ public class MyProfileFragment extends BaseProfileFragment {
         // Check if a collection is already in the viewModel, if not create the default one
         List<Collection> collections = viewModel.getCollections();
         if (collections == null) {
-            String defaultTitle = getResources().getString(R.string.recently_watched);
-            Collection defaultCollection = new Collection(defaultTitle);
-            collections = new ArrayList<>();
-            collections.add(defaultCollection);
-            CollectionsDatabase.addCollection(USERNAME, defaultCollection);
-            viewModel.setCollections(collections);
+            collections = createDefaultCollection();
         }
 
         // Define what happens when the add button inside a collection is clicked
         OnAddMediaButtonClickListener onAddMediaButtonClickListener = (collection, review) -> {
-       /*     CollectionsDatabase.addReviewToCollection(USERNAME, collection.getCollectionName(), review);*/
             String collectionName = collection.getCollectionName();
-            System.out.println("clicked collection name is: " +  collection.getCollectionName());
-            Collection currentCollection = viewModel.getCollection(collectionName);
 
-           // ((MyProfileViewModel)viewModel).addReviewToCollection(review, collectionName);
-
-            Bundle args = new Bundle();
-
-            // Give the username as an argument to the profile page and switch to it
+            // Pass the name of the collection to add the review to to the search fragment and switch to it
             SearchFragment searchFragment = new SearchFragment();
+            Bundle args = new Bundle();
             args.putString("collection", collectionName);
-            System.out.println("Collection name in 0: " + collectionName);
             searchFragment.setArguments(args);
             fragmentSwitcher.switchCurrentFragmentWithChildFragment(searchFragment);
         };
@@ -133,6 +111,27 @@ public class MyProfileFragment extends BaseProfileFragment {
         recyclerView.setLayoutManager(
                 new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         return collectionsAdapter;
+    }
+
+    private void addReview(){
+        Bundle args = getArguments();
+        String reviewSerialized = args.getString("review");
+        String collectionName = args.getString("collection");
+        Review review = new Gson().fromJson(reviewSerialized, Review.class);
+
+        if (review != null && collectionName != null) {
+            ((MyProfileViewModel)viewModel).addReviewToCollection(review, collectionName);
+        }
+    }
+
+    private List<Collection> createDefaultCollection(){
+        String defaultTitle = getResources().getString(R.string.recently_watched);
+        Collection defaultCollection = new Collection(defaultTitle);
+        List<Collection> collections = new ArrayList<>();
+        collections.add(defaultCollection);
+        CollectionsDatabase.addCollection(USERNAME, defaultCollection);
+        viewModel.setCollections(collections);
+        return collections;
     }
 
     private void setupAddCollectionsButton(Button addCollectionButton) {
