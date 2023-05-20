9dabe49cc41a8fa6ddc9bd13de76ca6d92473c8b
Simone Kalbermatter <simone.kalbermatter@epfl.ch>
2023-05-20 16:44:29 +0200

    refactor function to fix code smell

--- app/src/main/java/com/github/sdp/mediato/ui/CreateProfileFragment.java
+++ app/src/main/java/com/github/sdp/mediato/ui/CreateProfileFragment.java
@@ -49,7 +49,7 @@ public class CreateProfileFragment extends Fragment {
   private PhotoPicker photoPicker;
   private final User.UserBuilder userBuilder;
 
-  CreateProfileFragment(String uid, String email) {
+  public CreateProfileFragment(String uid, String email) {
     userBuilder = new User.UserBuilder(uid).setEmail(email);
   }
 
@@ -105,29 +105,33 @@ public class CreateProfileFragment extends Fragment {
     return view -> {
       String errorMsg = getUsernameErrorMsg(usernameEditText.getText());
       usernameTextInput.setError(errorMsg);
+
       if (null == errorMsg) {
-        //TODO Add navigation to Add favorite page
-        //Creating new user and adding it to the database
         String username = Objects.requireNonNull(usernameEditText.getText()).toString();
-        userBuilder.setUsername(username);
-        userBuilder.setRegisterDate(Dates.getToday());
-        //@TODO by default the location is not set - to be changed when we implement the GPS feature
-        userBuilder.setLocation(new Location());
-        User user = userBuilder.build();
-        Uri profilePicUri = photoPicker.getProfileImageUri();
-        UserDatabase.addUser(user);
-        if (photoPicker.getProfileImageUri() != null) {
-          uploadProfilePicTask = UserDatabase.setProfilePic(user.getUsername(), profilePicUri).addOnCompleteListener(v ->  {
-              switchToMainActivity(username);
-              makeToast(getString(R.string.profile_creation_success));
-          });
-
-        } else {
-          addDefaultProfilePic(username);
-        }
+        createUser(username);
       }
-
     };
+  }
+
+  private void createUser(String username){
+    // Create a new user and add it to the database
+    userBuilder.setUsername(username);
+    userBuilder.setRegisterDate(Dates.getToday());
+    userBuilder.setLocation(new Location());
+    User user = userBuilder.build();
+    UserDatabase.addUser(user);
+
+    // Set profile picture
+    Uri profilePicUri = photoPicker.getProfileImageUri();
+    if (photoPicker.getProfileImageUri() != null) {
+      uploadProfilePicTask = UserDatabase.setProfilePic(user.getUsername(), profilePicUri).addOnCompleteListener(v ->  {
+        switchToMainActivity(username);
+        makeToast(getString(R.string.profile_creation_success));
+      });
+
+    } else {
+      addDefaultProfilePic(username);
+    }
   }
 
   /**
